c24294c8072df95f4924956639f6ad9ae09af18e
Kevin James Baxter <meta.data@verizon.net>
2026-07-08 09:01:54 -0400

    commit

--- FlashLEDs/main.py
+++ FlashLEDs/main.py
@@ -1,10 +1,18 @@
 import uasyncio as asyncio
  
-async def hello():
-    print("Hello")
-    await asyncio.sleep(1)
-    print("World")
+async def task1():
+    for i in range(3):
+        print("Task 1:", i)
+        await asyncio.sleep(0.5)
+ 
+async def task2():
+    for i in range(3):
+        print("Task 2:", i)
+        await asyncio.sleep(0.3)
  
 async def main():
-    task = asyncio.create_task(hello())
-    await task
+    t1 = asyncio.create_task(task1())
+    t2 = asyncio.create_task(task2())
+    await asyncio.gather(t1, t2)
+ 
+asyncio.run(main())
